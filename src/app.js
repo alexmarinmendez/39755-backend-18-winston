82f3f243260bec76c98c333514213f99b1104f2b
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-07-15 10:19:24 -0500

    feat: add winston as a middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import express from 'express'
+import { loggermid } from './utils.js'
 
 const app = express()
+app.use(loggermid)
 
-app.get('/', (req, res) => res.json({ message: 'Maraton de Winston Logger' }))
+app.get('/', (req, res) => {
+    req.logger.info('Mensaje de info...')
+    res.json({ message: 'Maraton de Winston Logger' })
+})
 
 app.get('/testinglog', (req, res) => res.json({ message: 'Logger test...' }))
 
